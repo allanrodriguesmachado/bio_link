5cde91906a84b4921934d4f19bfc73c3cbdc1ddc
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-07-22 22:06:03 -0300

    Configurando Links

---
+++ app/Http/Controllers/LinksController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLinksRequest;
+use App\Http\Requests\UpdateLinksRequest;
+use App\Models\Links;
+
+class LinksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLinksRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Links $links)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Links $links)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLinksRequest $request, Links $links)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Links $links)
+    {
+        //
+    }
+}
